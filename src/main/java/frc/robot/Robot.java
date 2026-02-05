4b8342a14a03c62fa70058bfae32e8d861cf376f
Aditya-2204 <william1binki@gmail.com>
2026-02-05 15:56:54 -0600

    adding Patribots code MK5n

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,118 +1,206 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DataLogManager;
+import java.util.Optional;
+
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.util.Constants.AutoConstants;
+import frc.robot.util.Constants.DriveConstants;
+import frc.robot.util.Constants.KrakenMotorConstants;
+import frc.robot.util.Constants.LoggingConstants;
+import frc.robot.util.Constants.NeoMotorConstants;
+import frc.robot.util.hardware.phoenix.Kraken;
+import frc.robot.util.hardware.rev.Neo;
+import frc.robot.util.hardware.rev.NeoPhysicsSim;
 
 /**
- * The methods in this class are called automatically corresponding to each
- * mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the
- * package after creating
- * this project, you must also update the Main.java file in the project.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-  private final RobotContainer m_robotContainer;
+public class Robot extends LoggedRobot {
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any
-   * initialization code.
-   */
-  public Robot() {
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-    // and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-  }
+    private static Optional<Alliance> alliance = Optional.empty();
+    public static GameMode gameMode = GameMode.DISABLED;
+    public static enum GameMode {
+        DISABLED,
+        AUTONOMOUS,
+        TELEOP,
+        TEST
+    };
 
-  @Override
-  public void robotInit() {
+    public static double currentTimestamp = 0;
+    public static double previousTimestamp = 0;
 
-    // Record all NetworkTables entries
-    DataLogManager.logNetworkTables(true);
+    private Command autonomousCommand;
 
-    // Record Driver Station data (mode, alliance, etc.)
-    DriverStation.startDataLog(DataLogManager.getLog());
-  }
+    private RobotContainer robotContainer;
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items
-   * like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-public void robotPeriodic() {
-    // Run command scheduler
-    CommandScheduler.getInstance().run();
-}
+    @Override
+    public void robotInit() { 
+        // Git metadata for tracking version for AKit
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {
-  }
+        switch (LoggingConstants.getMode()) {
+            case REAL:
+                Logger.addDataReceiver(new WPILOGWriter("/media/sda1/logs")); // Log to a USB stick
+                Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+                break;
+            case REPLAY:
+                String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+                Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+                Logger.addDataReceiver(new WPILOGWriter(LogFileUtil
+                    .addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+                    break;
+            case SIM:
+                Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+                break;
+        }
+        
+        Logger.start(); 
 
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /**
-   * This autonomous runs the autonomous command selected by your
-   * {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+        robotContainer = new RobotContainer();
+        DriverStation.silenceJoystickConnectionWarning(true);
     }
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    /**
+     * This function is called every 20 ms, no matter the mode. Used for items like diagnostics
+     * ran during disabled, autonomous, teleoperated and test. :D
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        Robot.previousTimestamp = Robot.currentTimestamp;
+        Robot.currentTimestamp = Timer.getFPGATimestamp();
+        CommandScheduler.getInstance().run();
     }
-  }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-  }
+    @Override
+    public void disabledInit() {
+        Robot.gameMode = GameMode.DISABLED;
+        robotContainer.onDisabled();
+    }
+    
+    @Override
+    public void disabledPeriodic() {
+        // Now while this may not necessarily be a constant...
+        // it needs to be updated.
+        DriverStation.refreshData();
+        Robot.alliance = DriverStation.getAlliance();
+    }
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledExit() {
+        // Shut off NetworkTables broadcasting for most logging calls
+        // if we are at competition
+        RobotContainer.gameModeStart = currentTimestamp;
+        // Monologue.setFileOnly(DriverStation.isFMSAttached());
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
+    @Override   
+    public void autonomousInit() {
+        DriveConstants.MAX_SPEED_METERS_PER_SECOND = AutoConstants.MAX_SPEED_METERS_PER_SECOND;
+        Robot.gameMode = GameMode.AUTONOMOUS;
+        robotContainer.onEnabled();
+        // We only need to update alliance becuase
+        // sim GUI starts the bot in a "disconnected"
+        // state which won't update the alliance before
+        // we enable...
+        DriverStation.refreshData();
+        Robot.alliance = DriverStation.getAlliance();
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
+        autonomousCommand = robotContainer.getAutonomousCommand();
 
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
+        if (autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(autonomousCommand);
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void autonomousExit() {
+        // Stop our autonomous command if it is still running.
+        System.out.printf(
+            "*** Auto finished in %.2f secs ***%n", Robot.currentTimestamp - RobotContainer.gameModeStart);
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void teleopInit() {
+        Robot.gameMode = GameMode.TELEOP;
+        robotContainer.onEnabled();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void teleopExit() {}
+
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        Robot.gameMode = GameMode.TEST;
+        CommandScheduler.getInstance().cancelAll();
+        robotContainer.onEnabled();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void testExit() {
+        // Switch back to the normal button loop!
+        CommandScheduler.getInstance().setActiveButtonLoop(CommandScheduler.getInstance().getDefaultButtonLoop());
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        NeoPhysicsSim.getInstance().run();
+        Robot.alliance = DriverStation.getAlliance();
+
+        for (Neo neo : NeoMotorConstants.NEO_MOTOR_MAP.values()) {
+            neo.tick();
+        }
+
+        for (Kraken kraken : KrakenMotorConstants.KRAKEN_MOTOR_MAP.values()) {
+            kraken.tick();
+        }   
+    }
+
+    public static boolean isRedAlliance() {
+        return alliance.equals(Optional.of(Alliance.Red));
+    }
+
+    public static boolean isBlueAlliance() {
+        return alliance.equals(Optional.of(Alliance.Blue));
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,84 +1,282 @@
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.*;
-import edu.wpi.first.wpilibj2.command.*;
-import edu.wpi.first.wpilibj2.command.button.*;
-import frc.robot.subsystems.*;
-import frc.robot.utils.Constants;
-import frc.robot.utils.Constants.JoystickConstants;
-import frc.robot.controls.*;
-import frc.robot.controls.TurretTCs.TurretVelocityCommand;
+import java.util.function.BooleanSupplier;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Robot.GameMode;
+import frc.robot.commands.characterization.FeedForwardCharacterization;
+import frc.robot.commands.characterization.StaticCharacterization;
+import frc.robot.commands.characterization.WheelRadiusCharacterization;
+import frc.robot.commands.drive.Drive;
+import frc.robot.commands.logging.NTGainTuner;
+import frc.robot.commands.managers.HDCTuner;
+import frc.robot.subsystems.drive.Swerve;
+import frc.robot.util.Constants.AutoConstants;
+import frc.robot.util.Constants.OIConstants;
+import frc.robot.util.auto.PathPlannerStorage;
+import frc.robot.util.custom.PatriBoxController;
 
 public class RobotContainer {
-  public static final Joystick controller3D = new Joystick(0);
-  public static final Joystick wolfByte = new Joystick(1);
-  public static double pov;
-  public static final JoystickButton resetHeading_Start = new JoystickButton(controller3D, Constants.JoystickConstants.BaseRM);
-  private final Drivetrain drivetrain = Drivetrain.getInstance();
-  private final SpecDrive specDrive = SpecDrive.getInstance();
-  private final TurretSubsystem turret = TurretSubsystem.getInstance();
-  private final WolfSend wolfSend = WolfSend.getInstance();
-  private final WolfPoseEstimator wolfPoseEstimator = WolfPoseEstimator.getInstance();
-  private ParallelRaceGroup swerveStopCmd;
-  SendableChooser<Command> auton_chooser;
-  
-  public RobotContainer() {
-    CameraServer.startAutomaticCapture(0);
-    CameraServer.startAutomaticCapture(1);
-  
-    configureBindings();
-  
-    // swerveStopCmd = new SwerveDriveCommands(0.0,0.0,0.0).withTimeout(3);
-    // NamedCommands.registerCommand("Swerve Stop", swerveStopCmd);
-  
-    // auton_chooser = new SendableChooser<>();
-    // auton_chooser.setDefaultOption("MidReefAuto", new PathPlannerAuto("MidReefAuto"));
-    // SmartDashboard.putData("Auton Chooser", auton_chooser);
-  }
 
-  private void configureBindings() {
-    double slider = (-RobotContainer.controller3D.getRawAxis(JoystickConstants.Slider) + 1) / 2.0;
-    if (slider == 0)  {
-      slider = 0.001;
+    private PowerDistribution pdh;
+
+    private EventLoop testButtonBindingLoop = new EventLoop();
+
+    private final PatriBoxController driver;
+    @SuppressWarnings("unused")
+    private final PatriBoxController operator;
+
+    private boolean fieldRelativeToggle = true;
+    private final BooleanSupplier robotRelativeSupplier;
+
+    private final Swerve swerve;
+
+    public static Field2d field2d = new Field2d();
+
+    private PathPlannerStorage pathPlannerStorage;
+    private static HDCTuner HDCTuner;
+
+    // Draggables
+    @AutoLogOutput(key = "Draggables/FreshCode")
+    public static boolean freshCode = true;
+    @AutoLogOutput(key = "Draggables/RobotPose2d")
+    public static Pose2d robotPose2d = new Pose2d();
+    @AutoLogOutput(key = "Draggables/RobotPose3d")
+    public static Pose3d robotPose3d = new Pose3d();
+    @AutoLogOutput(key = "Draggables/SwerveMeasuredStates")
+    public static SwerveModuleState[] swerveMeasuredStates;
+    @AutoLogOutput(key = "Draggables/SwerveDesiredStates")
+    public static SwerveModuleState[] swerveDesiredStates;
+    @AutoLogOutput(key = "Draggables/GameModeStart")
+    public static double gameModeStart = 0;
+
+    public RobotContainer() {
+
+        System.out.println("Constructing Robot Container...");
+
+        driver = new PatriBoxController(OIConstants.DRIVER_CONTROLLER_PORT, OIConstants.DRIVER_DEADBAND);
+        operator = new PatriBoxController(OIConstants.OPERATOR_CONTROLLER_PORT, OIConstants.OPERATOR_DEADBAND);
+
+        pdh = new PowerDistribution(30, ModuleType.kRev);
+        pdh.setSwitchableChannel(false);
+
+        swerve = new Swerve();
+
+        SmartDashboard.putData(field2d);
+
+        driver.back().toggleOnTrue(
+                Commands.runOnce(() -> fieldRelativeToggle = !fieldRelativeToggle)
+        );
+        robotRelativeSupplier = () -> fieldRelativeToggle;
+
+        swerve.setDefaultCommand(new Drive(
+                swerve,
+                driver::getLeftY,
+                driver::getLeftX,
+                () -> -driver.getRightX() / 1.6,
+                robotRelativeSupplier,
+                () -> (robotRelativeSupplier.getAsBoolean() && Robot.isRedAlliance())
+        ));
+
+        HDCTuner = new HDCTuner(
+                AutoConstants.HDC.getXController(),
+                AutoConstants.HDC.getThetaController());
+
+        configureButtonBindings();
+        configureTimedEvents();
+
+        pathPlannerStorage = new PathPlannerStorage();
+
+        pathPlannerStorage.configureAutoChooser();
+        pathPlannerStorage.getAutoChooser().addOption("WheelRadiusCharacterization",
+                swerve.setWheelsOCommand()
+                        .andThen(Commands.waitSeconds(0.5))
+                        .andThen(new WheelRadiusCharacterization(swerve)));
+        pathPlannerStorage.getAutoChooser().addOption("DriveFeedForwardCharacterization",
+                new FeedForwardCharacterization(
+                        swerve,
+                        swerve::runDriveCharacterization,
+                        swerve::getDriveCharacterizationVelocity));
+        pathPlannerStorage.getAutoChooser().addOption("TurnStaticCharacterization",
+                new StaticCharacterization(
+                        swerve,
+                        swerve::runTurnCharacterization,
+                        swerve::getTurnCharacterizationVelocity));
+
+        CommandScheduler.getInstance().schedule(new NTGainTuner());
+
+        prepareNamedCommands();
+
     }
 
-    double frontSpeed = RobotContainer.controller3D.getRawAxis(JoystickConstants.X) * slider;
-    double sideSpeed = RobotContainer.controller3D.getRawAxis(JoystickConstants.Y) * slider;
-    double turnSpeed = RobotContainer.controller3D.getRawAxis(JoystickConstants.Rot) * slider;
-
-
-
-    JoystickButton turretTestButton = new JoystickButton(wolfByte, 3); // Button 3
-    turretTestButton.onTrue(
-        new SequentialCommandGroup(
-          new TurretVelocityCommand(turret, Math.PI / 4).withTimeout(4.0),
-          new TurretVelocityCommand(turret, -Math.PI/4).withTimeout(4.0)
-        )
-    );
-
-    // Must rotate CCW and revolve once;
-
-    drivetrain.setDefaultCommand(new SwerveDriveCommands(frontSpeed,sideSpeed,turnSpeed));
-
-    resetHeading_Start.onTrue(new InstantCommand(drivetrain::zeroHeading, drivetrain));
-
-    //Comment out before driving. Will only let robot turn.
-    pov = wolfByte.getPOV();
-    if (pov != -1) {
-      specDrive.setDefaultCommand(new SpecDriveCommands(wolfByte.getPOV()));
+    private void configureButtonBindings() {
+        configureDriverBindings(driver);
+        configureOperatorBindings(operator);
     }
-    //specDrive.setDefaultCommand(new SpecDriveCommands2(wolfByte.getRawAxis(0)));
-  }
 
-  public Command getAutonomousCommand() {
-    return auton_chooser.getSelected();
-  }
+    private void configureTimedEvents() {
+    }
 
-  public Drivetrain getDrivetrain() {
-    return drivetrain;
-  }
-} //Nice - Wolfram121
+    private void configureDriverBindings(PatriBoxController controller) {
+
+        controller.start().onTrue(
+                Commands.runOnce(() -> swerve.resetOdometry(
+                new Pose2d(
+                        swerve.getPose().getTranslation(),
+                        Rotation2d.fromDegrees(
+                                Robot.isRedAlliance()
+                                ? 0
+                                : 180))
+        ), swerve)
+        );
+
+        controller.leftBumper().whileTrue(swerve.getSetWheelsX());
+        controller.rightBumper().whileTrue(swerve.getSetWheelsO());
+        controller.b().whileTrue(swerve.driveCharacterization());
+        controller.y().whileTrue(swerve.getSetWheelsZero());
+    }
+
+    private void configureOperatorBindings(PatriBoxController controller) {
+
+        // controller.povUp()
+        //     .whileTrue(krakenTest.setPosition(() -> krakenTest.getPosition() + 0.2));
+        // controller.povDown()
+        //     .whileTrue(krakenTest.setPosition(() -> krakenTest.getPosition() - 0.2));
+        // controller.a()
+        //     .onTrue(krakenTest.setPosition(() -> 0));
+        // controller.b()
+        //     .whileTrue(krakenTest.setVelocity(() -> 500)
+        //         .finallyDo(() -> krakenTest.setVelocity(() -> 0)));
+        // controller.x()
+        //     .whileTrue(krakenTest.setVelocity(() -> -500)
+        //         .finallyDo(() -> krakenTest.setVelocity(() -> 0)));
+        // controller.rightTrigger()
+        //     .whileTrue(krakenTest.setPercent(controller::getRightY)
+        //         .finallyDo(() -> krakenTest.setPercent(() -> 0)));
+    }
+
+    public void updateNTGains() {
+        double PPHDCP = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/0-P").getDouble(-1);
+        double PPHDCI = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/1-I").getDouble(-1);
+        double PPHDCD = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/2-D").getDouble(-1);
+        double PPHDCP2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/0-P").getDouble(-1);
+        double PPHDCI2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/1-I").getDouble(-1);
+        double PPHDCD2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/2-D").getDouble(-1);
+
+        double HDCP = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/0-P").getDouble(-1);
+        double HDCI = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/1-I").getDouble(-1);
+        double HDCD = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/2-D").getDouble(-1);
+        double HDCP2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/0-P").getDouble(-1);
+        double HDCI2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/1-I").getDouble(-1);
+        double HDCD2 = NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/2-D").getDouble(-1);
+
+        if (PPHDCP == -1 || PPHDCI == -1 || PPHDCD == -1 || PPHDCP2 == -1 || PPHDCI2 == -1 || PPHDCD2 == -1
+                || HDCP == -1 || HDCI == -1 || HDCD == -1 || HDCP2 == -1 || HDCI2 == -1 || HDCD2 == -1) {
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/0-P").setDouble(AutoConstants.XY_CORRECTION_P);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/1-I").setDouble(AutoConstants.XY_CORRECTION_I);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Translation/2-D").setDouble(AutoConstants.XY_CORRECTION_D);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/0-P").setDouble(AutoConstants.ROTATION_CORRECTION_P);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/1-I").setDouble(AutoConstants.ROTATION_CORRECTION_I);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("Auto/Rotation/2-D").setDouble(AutoConstants.ROTATION_CORRECTION_D);
+
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/0-P").setDouble(AutoConstants.XY_CORRECTION_P);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/1-I").setDouble(AutoConstants.XY_CORRECTION_I);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Translation/2-D").setDouble(AutoConstants.XY_CORRECTION_D);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/0-P").setDouble(AutoConstants.ROTATION_CORRECTION_P);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/1-I").setDouble(AutoConstants.ROTATION_CORRECTION_I);
+            NetworkTableInstance.getDefault().getTable("Calibration").getEntry("HDC/Rotation/2-D").setDouble(AutoConstants.ROTATION_CORRECTION_D);
+            return;
+        } else {
+            AutoConstants.PPHDC = new PPHolonomicDriveController(
+                    new PIDConstants(
+                            PPHDCP,
+                            PPHDCI,
+                            PPHDCD),
+                    new PIDConstants(
+                            PPHDCP2,
+                            PPHDCI2,
+                            PPHDCD2));
+
+            AutoConstants.XY_PID.setP(HDCP);
+            AutoConstants.XY_PID.setI(HDCI);
+            AutoConstants.XY_PID.setD(HDCD);
+            AutoConstants.THETA_PID.setP(HDCP2);
+            AutoConstants.THETA_PID.setI(HDCI2);
+            AutoConstants.THETA_PID.setD(HDCD2);
+
+        }
+    }
+
+    public Command getAutonomousCommand() {
+        return pathPlannerStorage.getSelectedAuto();
+    }
+
+    private void configureHDCBindings(PatriBoxController controller) {
+        controller.pov(0, 270, testButtonBindingLoop)
+                .onTrue(HDCTuner.controllerDecrementCommand());
+
+        controller.pov(0, 90, testButtonBindingLoop)
+                .onTrue(HDCTuner.controllerIncrementCommand());
+
+        controller.pov(0, 0, testButtonBindingLoop)
+                .onTrue(HDCTuner.increaseCurrentConstantCommand(.1));
+
+        controller.pov(0, 180, testButtonBindingLoop)
+                .onTrue(HDCTuner.increaseCurrentConstantCommand(-.1));
+
+        controller.rightBumper(testButtonBindingLoop)
+                .onTrue(HDCTuner.constantIncrementCommand());
+
+        controller.leftBumper(testButtonBindingLoop)
+                .onTrue(HDCTuner.constantDecrementCommand());
+
+        controller.a(testButtonBindingLoop)
+                .onTrue(HDCTuner.logCommand());
+
+        controller.x(testButtonBindingLoop)
+                .onTrue(HDCTuner.multiplyPIDCommand(2));
+
+        controller.b(testButtonBindingLoop)
+                .onTrue(HDCTuner.multiplyPIDCommand(.5));
+    }
+
+    public void onDisabled() {
+        swerve.stopDriving();
+        CommandScheduler.getInstance().schedule(pathPlannerStorage.updatePathViewerCommand());
+        pathPlannerStorage.configureAutoChooser();
+
+        // TODO: Extract this into a command file
+        CommandScheduler.getInstance().schedule(
+                Commands.run(this::updateNTGains)
+                .until(() -> Robot.gameMode != GameMode.DISABLED)
+                .ignoringDisable(true)
+                );
+    }
+
+    public void onEnabled() {
+        gameModeStart = Robot.currentTimestamp;
+        CommandScheduler.getInstance().schedule(pathPlannerStorage.updatePathViewerCommand());
+        freshCode = false;
+    }
+
+    private void prepareNamedCommands() {
+    }
+
+}
