8b156aa3160f4a201f235e3f9a855627aa462a9c
Brainspark1 <165429616+Brainspark1@users.noreply.github.com>
2026-02-15 13:28:55 -0600

    Fixed motor ids

--- src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.Intake;
 
 public class IntakeConstants 
 {
-    public static final int INTAKE_MOTOR_ID_1 = 9;
-    public static final int INTAKE_MOTOR_ID_2 = 10;
+    public static final int INTAKE_MOTOR_ID_1 = 10;
+    public static final int INTAKE_MOTOR_ID_2 = 11;
     public static final double SPEEDF = 0.9;
 }
