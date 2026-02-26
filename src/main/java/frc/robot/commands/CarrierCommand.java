c24f6d26c7bcbd00307354fe22d4dafbdb8ea125
Aditya-2204 <william1binki@gmail.com>
2026-02-25 23:47:20 -0600

    updating carrier command

--- src/main/java/frc/robot/commands/CarrierCommand.java
+++ src/main/java/frc/robot/commands/CarrierCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -8,10 +9,10 @@ import frc.robot.subsystems.Outtake.Outtake;
 import frc.robot.subsystems.Turret.Turret;
 
 public class CarrierCommand extends Command{
-    private Double speed;
+    private DoubleSupplier speed;
     private Outtake outtake;
     
-        public CarrierCommand(Outtake outtake, Double speed){
+        public CarrierCommand(Outtake outtake, DoubleSupplier speed){
             this.speed=speed;
             this.outtake=outtake;
         addRequirements(outtake);
@@ -19,8 +20,8 @@ public class CarrierCommand extends Command{
 
     @Override
     public void execute(){
-        outtake.setPullRotation(speed);
-        outtake.setCarryVelocity(speed);
+        outtake.setPullRotation(speed.getAsDouble());
+        outtake.setCarryVelocity(speed.getAsDouble());
         
         
     }
